ebaa786a4795b17e9f0a835b20c734c8dd3bb7ea
nesso <32949286+nesso-pfl@users.noreply.github.com>
2020-08-09 11:05:08 +0900

    Add abc122 and solve a part of it

---
+++ abc122/src/bin/c.rs
@@ -0,0 +1,62 @@
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let s = {
+            use std::io::Read;
+            let mut s = String::new();
+            std::io::stdin().read_to_string(&mut s).unwrap();
+            s
+        };
+        let mut iter = s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($iter:expr) => {};
+    ($iter:expr, ) => {};
+
+    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($iter:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($iter, $t)),* )
+    };
+
+    ($iter:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+    };
+
+    ($iter:expr, chars) => {
+        read_value!($iter, String).chars().collect::<Vec<char>>()
+    };
+
+    ($iter:expr, usize1) => {
+        read_value!($iter, usize) - 1
+    };
+
+    ($iter:expr, $t:ty) => {
+        $iter.next().unwrap().parse::<$t>().expect("Parse error")
+    };
+}
+
+fn main() {
+    input!{
+        n: usize,
+        q: u32,
+        s: chars,
+        lr: [[usize; 2]; q]
+    }
+    let mut m: [u32; 100009] = [0; 100009];
+    (2..n+1).into_iter().for_each(|i| m[i] = if s[i-2] == 'A' && s[i-1] == 'C' { m[i-1] + 1 } else { m[i-1] }); 
+    lr.into_iter().for_each(|lr_| {
+        println!("{}", m[lr_[1]] - m[lr_[0]]);
+    })
+}
